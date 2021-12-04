9b77785f61ab2b069b4409059a9f67c227cb3c4c
Newburn, Josh <newburn2@illinois.edu>
2021-12-04 10:14:25 +0100

    Create Graph.h

---
+++ Graph.h
@@ -0,0 +1,13 @@
+class Graph { #Graph.h
+Public:
+	Graph();
+	Void init(ifstream file);
+	
+	Std::vector<Edge> getEdges();
+	Std::vector<City> getCities();
+	Private:
+		std::vector<std::vector<double>> FW();
+		std::vector<std::vector<double>> dijkstras(); // change vectors
+		std::vector<Edge> edges_;
+		std::vector<City> cities_;
+} # Graph.cpp
