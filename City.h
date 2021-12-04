0bb9f68be59fa71846fa54c9ef2d6a625fbfba47
Newburn, Josh <newburn2@illinois.edu>
2021-12-04 10:13:07 +0100

    Create City.h

---
+++ City.h
@@ -0,0 +1,10 @@
+class City { #City.h
+	Public:
+		City();
+		City(string name);
+		String getName();
+		map<string, double> getAdj();
+	Private:
+		Std::string _city_name;
+		std::map<string, double> adj_cities;
+} #implement this stuff in City.cpp
