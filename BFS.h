f8ae5ffa136da56a6850e9d80f9fc51dcf691845
Newburn, Josh <newburn2@illinois.edu>
2021-12-04 10:19:17 +0100

    Create BFS.h

---
+++ BFS.h
@@ -0,0 +1,24 @@
+std::vector<std::vector<double>> BFS (Graph G, City s, City e) {
+	std::queue<City> q; # queue
+	std::vector<City> path;
+	std::map<string, bool> explored;
+	std::map<string, bool> previous;
+	for (City v : cities_) {
+		explored[v] = false;
+		previous[v] = NULL;
+	}
+	explored[s] = true;
+	q.push(s);
+	while (!q.empty()) {
+		If (v == e) {
+			path.push_back(v);
+			return path;
+		}
+		q.pop();
+		for (City adjCity: Cities[v]) {
+			If (!explored[v]) {
+				explored[v] = true;
+				q.push(v);
+		}
+	}
+}
